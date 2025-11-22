df3d269eca500e70c9392fe49ad338b052cf2e3b
atomnoid <aayushs2580@gmail.com>
2025-11-22 17:26:53 +0530

    fixed the error of text cut

--- src/main.ts
+++ src/main.ts
@@ -29,42 +29,41 @@ myForm.onsubmit = (e: SubmitEvent) => {
   todoInput.value = "";
   renderTodo(todos);
 };
-
-const generateTodoItem = (title: string, isCompleted: boolean, id: String) => {
+const generateTodoItem = (title: string, isCompleted: boolean, id: string) => {
   const todo: HTMLDivElement = document.createElement("div");
   todo.className = "todo";
 
-  // created a checkbox
+  // checkbox
   const checkBox: HTMLInputElement = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
+  checkBox.type = "checkbox";
   checkBox.className = "isCompleted";
   checkBox.checked = isCompleted;
+
+  // title paragraph
+  const paragraph: HTMLParagraphElement = document.createElement("p");
+  paragraph.innerText = title;
+  paragraph.className = isCompleted ? "textCut" : "";
+
   checkBox.onchange = () => {
+    // update TS array
     todos.find((item) => {
       if (item.id === id) item.isComplete = checkBox.checked;
     });
 
-    paragraph.className = isCompleted ? "textCut" : "";
+    // update UI immediately
+    paragraph.className = checkBox.checked ? "textCut" : "";
   };
 
-  //created p for title
-  //const paragraph: HTMLParagraphElement = document.createElement("input");
-  //paragraph.innerText = title;
-  const paragraph = document.createElement("p");
-  paragraph.innerText = title;
-
-  paragraph.className = isCompleted ? "textCut" : "";
-
-  // created a delete button
+  // delete btn
   const button: HTMLButtonElement = document.createElement("button");
   button.className = "deleteBtn";
   button.innerText = "X";
   button.onclick = () => deleteTodo(String(id));
 
-  // Append all elements to todo div
   todo.append(checkBox, paragraph, button);
   todosContainer.append(todo);
 };
+
 
 const deleteTodo = (id: string) => {
   const idx = todos.findIndex((item) => item.id === id);
